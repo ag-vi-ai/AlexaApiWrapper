601bb139d9715bfd16217cee78569691bd756fa9
Andrew Gubskiy <andrew.gubskiy@gmail.com>
2017-07-15 18:09:35 +0300

    Added client project

---
+++ VI.AWS.AlexaTopSites.Client/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VI.AWS.AlexaTopSites.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string key = Environment.GetEnvironmentVariable("AWS_ALEXA_KEY");
+            string secretKey = Environment.GetEnvironmentVariable("AWS_ALEXA_SECRET_KEY");
+
+            var client = new VI.AWS.AlexaTopSites.AlexaClient(key, secretKey);            
+            var result = client.GetUrlInfo("https://torf.tv", UrlInfoResponseGroup.All);
+
+            Console.WriteLine(result.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
